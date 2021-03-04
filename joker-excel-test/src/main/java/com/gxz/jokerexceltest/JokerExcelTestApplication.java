89338ee6e0a224cb1d4395abb73b43302f1df425
gxz <514190950@qq.com>
2021-03-04 19:07:56 +0800

    增加测试内容

---
+++ joker-excel-test/src/main/java/com/gxz/jokerexceltest/JokerExcelTestApplication.java
@@ -0,0 +1,13 @@
+package com.gxz.jokerexceltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokerExcelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokerExcelTestApplication.class, args);
+    }
+
+}
---
+++ joker-excel-test/src/test/java/com/gxz/jokerexceltest/JokerExcelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxz.jokerexceltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokerExcelTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
