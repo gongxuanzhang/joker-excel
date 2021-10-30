2861f5529f7f7f7dce3a7b3d4dfa0f17e851f125
gxz <514190950@qq.com>
2021-10-30 14:28:33 +0800

    修改CopyRow的复制空指针的问题

--- joker-excel-starter-autoconfigurer/src/main/java/org/gxz/joker/starter/tool/PoiUtils.java
+++ joker-excel-starter-autoconfigurer/src/main/java/org/gxz/joker/starter/tool/PoiUtils.java
@@ -1,8 +1,11 @@
 package org.gxz.joker.starter.tool;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 /**
  * @author gxz gongxuanzhang@foxmail.com
@@ -11,39 +14,44 @@ public class PoiUtils {
 
     public static void copyRow(Row source, Row target) {
         for (int i = 0; i < (int) source.getLastCellNum(); i++) {
-            Cell targetCell = target.createCell(i);
             Cell sourceCell = source.getCell(i);
-            CellType cellTypeEnum = sourceCell.getCellTypeEnum();
-            switch (cellTypeEnum) {
-                case STRING:
-                    targetCell.setCellValue(sourceCell.getStringCellValue());
-                    break;
-                case NUMERIC:
-                    targetCell.setCellValue(sourceCell.getNumericCellValue());
-                    break;
-                default:
-                    break;
+            if(sourceCell != null){
+                Cell targetCell = target.createCell(i);
+                Object cellValue = getCellValue(targetCell);
+                CellType cellTypeEnum = sourceCell.getCellTypeEnum();
+                switch (cellTypeEnum) {
+                    case STRING:
+                        targetCell.setCellValue((String) cellValue);
+                        break;
+                    case NUMERIC:
+                        targetCell.setCellValue((double) cellValue);
+                        break;
+                    case FORMULA:
+                        targetCell.setCellFormula((String) cellValue);
+                    default:
+                        break;
+                }
             }
         }
 
     }
 
-    public static void appendCell(Row row, String value){
+    public static void appendCell(Row row, String value) {
         short lastCellNum = row.getLastCellNum();
-        appendCell(row,value,lastCellNum);
+        appendCell(row, value, lastCellNum);
     }
 
-    public static void appendCell(Row row, String value,int cellIndex){
+    public static void appendCell(Row row, String value, int cellIndex) {
         Cell cell = row.createCell(cellIndex);
         cell.setCellValue(value);
     }
 
-    public static Object getCellValue(Cell cell){
-        if(cell == null){
+    public static Object getCellValue(Cell cell) {
+        if (cell == null) {
             return null;
         }
         CellType cellTypeEnum = cell.getCellTypeEnum();
-        switch (cellTypeEnum){
+        switch (cellTypeEnum) {
             case STRING:
                 return cell.getStringCellValue();
             case BOOLEAN:
@@ -52,6 +60,8 @@ public class PoiUtils {
                 return cell.getCellFormula();
             case NUMERIC:
                 return cell.getNumericCellValue();
+            case BLANK:
+                return "";
             default:
                 return null;
         }
